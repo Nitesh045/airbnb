39ff86b54e8513c91d0c8b025ec1107b65569649
Nitesh Yadav <niteshyadav604@gmail.com>
2024-02-13 16:06:20 +0530

    change file

--- backend/conn/db.js
+++ backend/conn/db.js
@@ -1,5 +1,9 @@
 const mongoose= require('mongoose');
 const dbUri= 'mongodb://0.0.0.0:27017/AirbnbProject';
-mongoose.connect(dbUri)
+mongoose.connect(dbUri,{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    family: 4,
+})
 .then(()=>console.log('db has connected '))
 .catch((e)=>console.log(e));
