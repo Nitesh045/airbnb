82a7eea5bd536e159f7653c3c4505e86f3fc117e
Nitesh Yadav <niteshyadav604@gmail.com>
2024-02-13 18:10:02 +0530

    change file

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -12,7 +12,7 @@ let storage = multer.diskStorage({
 let upload = multer({
     storage:storage,
     fileFilter:(req,file,cb)=>{
-        if(file.mimetype=='image/jpeg'|| file.mimetype=='image/jpg' || file.mimetype=='image/png' ||file.mimetype=='image/gif' ){
+        if(file.mimetype=='image/jpeg'|| file.mimetype=='image/jpg' || file.mimetype=='image/png' ||file.mimetype=='image/gif' ||file.mimetype=='image/webp' ){
             cb(null,true)
         }else{
             cb(null,false)
